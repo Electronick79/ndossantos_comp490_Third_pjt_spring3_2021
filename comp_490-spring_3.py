0dfdedd63cecf357c3d36cacabb792bb817b8a19
Electronick79 <ndossantos@student.brigew.edu>
2021-03-05 23:59:07 -0500

    Merge remote-tracking branch 'origin/master' into master

--- comp_490-spring_3.py
+++ comp_490-spring_3.py
@@ -1,6 +1,21 @@
 import sqlite3
 import pandas as pd
-#import openpyxl
+
+import mysql.connector
+db_connection = mysql.connector.connect(
+  host= "localhost",
+  user= "root",
+  passwd= "root"
+  )
+
+
+# creating database_cursor to perform SQL operation
+db_cursor = db_connection.cursor()
+# executing cursor with execute method and pass SQL query
+db_cursor.execute("CREATE DATABASE my_first_db")
+# get list of all databases
+db_cursor.execute("SHOW DATABASES")
+
 
 df = pd.read_csv(r'Path where the CSV file is stored\COMP490_SPRING_3.XLSX')
 print (df)
@@ -17,9 +32,18 @@ c.execute('''CREATE TABLE OCCUPATIONAL
 c.execute('''CREATE TABLE STATES
              ([generated_id] INTEGER PRIMARY KEY,[States_ID] integer, [States_Name] text)''')
 
-# Create table - DAILY_STATUS
-c.execute('''CREATE TABLE DAILY_STATUS
-             ([Client_Name] text, [Country_Name] text, [Date] date)''')
+# Create table - EMPLOYMENT
+c.execute('''CREATE TABLE EMPLOYMENT_STATUS
+             ([Employment_Name] text, [STATE_Name] text, [Date] date), [ Field_in_that_state] text''')
+
+# Create table - 25TH_PERCENTILE_SALARY
+c.execute('''CREATE TABLE 25TH_PERCENTILE_SALARY
+             ([25th_percent_salary] integer, [Employment_Name] text, [Date] date), [ Field_in_that_state] text''')
+
+# print all databases
+
+for db in db_cursor:
+	print(db)
 
 conn.commit()
 
@@ -28,6 +52,6 @@ conn.commit()
 
 
 
-#df = pd.DataFrame({'States':['California', 'Florida', 'Montana', 'Colorodo', 'Washington', 'Virginia'],
+#df = pd.DataFrame({'States':['California', 'Florida', 'Montana', 'Colorado', 'Washington', 'Virginia'],
    # 'Capitals':['Sacramento', 'Tallahassee', 'Helena', 'Denver', 'Olympia', 'Richmond'],
     #'Population':['508529', '193551', '32315', '619968', '52555', '227032']})
